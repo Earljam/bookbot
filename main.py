64d26d4ca54a9311bea0f9fc569f17ddcef12099
EarlJam <earlcoenterprises@gmail.com>
2024-12-19 10:28:35 -0800

    Pretty print a report

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 def main():
     book_path = "books/frankenstein.txt"
     full_text = open_book(book_path)
-    print(full_text)
+    print(f"--- Begin report of {book_path} ---")
     number_of_words = word_count(full_text)
-    print (number_of_words)
+    print(f"{number_of_words} words found in this document\n")
 
-    letter_counter(full_text)
-
+    letter_count = letter_counter(full_text)
+    
+    letter_count.sort(reverse=True, key=sort_aggregate)
+    for i in letter_count:
+        print(f"The '{i['letter']}' character was found {i['result']} times")
     
 
 def open_book(bookpath_string):
@@ -18,14 +21,22 @@ def word_count(text):
     return len(count)
 
 def letter_counter(book):
-    letter_count_dictionary = {}
+    
     alphabet = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
     letter_list = alphabet.split(',')
+    letter_count_list = []
     
     # filling out initial letter : count dictionary
     for letter in letter_list:
-        letter_count_dictionary[letter] = book.lower().count(letter)
+        letter_count_dictionary = {}
+        letter_count_dictionary["letter"] = letter
+        letter_count_dictionary["result"] = book.lower().count(letter)
+        letter_count_list.append(letter_count_dictionary)
 
-    print(letter_count_dictionary)
+    return(letter_count_list)
+
+    
+def sort_aggregate(aggregate):
+    return aggregate["result"]
 
 main()
