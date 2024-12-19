bfe42afb4d16a41eee3788a6d8ea2d1cd74ffb0e
EarlJam <earlcoenterprises@gmail.com>
2024-12-18 17:05:32 -0800

    Added word counts and letter counts

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
+    book_path = "books/frankenstein.txt"
+    full_text = open_book(book_path)
+    print(full_text)
+    number_of_words = word_count(full_text)
+    print (number_of_words)
 
-        print(file_contents)
+    letter_counter(full_text)
+
+    
+
+def open_book(bookpath_string):
+    with open(bookpath_string) as f:
+        return f.read()
+    
+def word_count(text):
+    count = text.split()
+    return len(count)
+
+def letter_counter(book):
+    letter_count_dictionary = {}
+    alphabet = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
+    letter_list = alphabet.split(',')
+    
+    # filling out initial letter : count dictionary
+    for letter in letter_list:
+        letter_count_dictionary[letter] = book.lower().count(letter)
+
+    print(letter_count_dictionary)
 
 main()
